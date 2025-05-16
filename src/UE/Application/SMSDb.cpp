d0e36e9dbf7b60c0c58332183ab4b9910f369a5c
JakubPalys <jakub.palys@student.pk.edu.pl>
2025-05-16 20:18:39 +0200

    added prototype functions to SMSDb.cpp

--- src/UE/Application/SMSDb.cpp
+++ src/UE/Application/SMSDb.cpp
@@ -1,33 +1,40 @@
 #include "SMSDb.hpp"
-#include "Messages/PhoneNumber.hpp"
-#include <map>
-#include <string>
-#include <vector>
-#include <optional>
+#include <stdexcept>
 
-namespace ue{
+namespace ue {
 
-    std::size_t addSMS(common::PhoneNumber from, const std::string& text) {
-        //TODO
-        return 0;
+std::size_t SmsDb::addSMS(common::PhoneNumber from, const std::string& text) {
+    messages.emplace_back(from, text);
+    lastSmsIndex = messages.size() - 1;
+    return *lastSmsIndex;
+}
+
+std::size_t SmsDb::addSentSMS(common::PhoneNumber to, const std::string& text, Sms::SmsStatus initialStatus) {
+    messages.emplace_back(to, text, initialStatus); 
+    lastSmsIndex = messages.size() - 1;
+    return *lastSmsIndex;
+}
+
+const std::vector<Sms>& SmsDb::getAllSMS() const {
+    return messages;
+}
+
+std::size_t SmsDb::getUnread() const {
+    std::size_t count = 0;
+    for (const auto& sms : messages) {
+        if (sms.status == Sms::SmsStatus::Unread && sms.direction == Sms::SmsDirection::Received) {
+            ++count;
+        }
     }
+    return count;
+}
 
-    std::size_t addSentSMS(common::PhoneNumber to, const std::string& text, Sms::SmsStatus initialStatus = Sms::SmsStatus::Unread) {
-        //TODO
-        return 0;
+bool SmsDb::markAsRead(std::size_t idx) {
+    if (idx >= messages.size()) {
+        return false; 
+    messages[idx].status = Sms::SmsStatus::Read;
+    return true;
     }
+}
 
-    const std::vector<Sms>& getAllSMS() {
-        //TODO
-    }
-
-    std::size_t getUnread() {
-        //TODO
-        return 0;
-    }
-
-    bool markAsRead(std::size_t idx) {
-        //TODO
-        return 0;
-    }
 }
